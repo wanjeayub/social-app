a6381328102dcc46df58358f28833c8a40e9d03d
Ayub Wanje <ayub.wanje@gmail.com>
2024-09-13 21:33:57 +0300

    Complete Header component - pending logo image

--- social-app-front/src/components/Header.js
+++ social-app-front/src/components/Header.js
@@ -1,17 +1,70 @@
 import styled from "styled-components";
 
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Flag";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+import { Avatar, IconButton } from "@material-ui/core";
+
 const Header = () => {
   return (
     <HeaderWrapper>
       <HeaderLeft>
         <img src="logo.png" alt="my social app" />
       </HeaderLeft>
+      <HeaderInput>
+        <SearchIcon />
+        <input placeholder="Search" type="text" />
+      </HeaderInput>
+      <HeaderCenter>
+        <div className="header__option header__option--active">
+          <HomeIcon fontsize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontsize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontsize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontsize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontsize="large" />
+        </div>
+      </HeaderCenter>
+      <HeaderRight>
+        <div className="header__option try">
+          <Avatar src="" />
+          <h4>Wanje</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </HeaderRight>
     </HeaderWrapper>
   );
 };
 
 const HeaderWrapper = styled.div`
   display: flex;
+  align-items: center;
   padding: 15px 20px;
   justify-content: space-between;
   position: sticky;
@@ -26,6 +79,67 @@ const HeaderLeft = styled.div`
   justify-content: space-evenly;
   img {
     height: 40px;
+  }
+`;
+const HeaderInput = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #eff2f5;
+  padding: 10px;
+  margin-left: 10px;
+  border-radius: 33px;
+  input {
+    border: none;
+    background-color: transparent;
+    outline-width: 0;
+  }
+`;
+
+const HeaderCenter = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  .header__option {
+    display: flex;
+    align-items: center;
+    padding: 10px 30px;
+    cursor: pointer;
+    .MuiSvgIcon {
+      color: grey;
+    }
+    &:hover {
+      background-color: #eff2f5;
+      border-radius: 10px;
+      align-items: center;
+      padding: 0 30px;
+      border-bottom: none;
+      .MuiSvgIcon-root {
+        color: #2e81f4;
+      }
+    }
+  }
+  .header__option--active {
+    border-bottom: 4px solid #2e81f4;
+    .MuiSvgIcon {
+      color: #2e81f4;
+    }
+  }
+`;
+
+const HeaderRight = styled.div`
+  display: flex;
+  .try {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 20px;
+  }
+  .header__info {
+    display: flex;
+    align-items: center;
+    h4 {
+      margin-left: 10px;
+    }
   }
 `;
 
